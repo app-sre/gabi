3d54e6db308bea585c239e76b5d1b79f8c3f6707
Krzysztof Wilczyński <kwilczynski@redhat.com>
2023-02-08 19:07:34 +0900

    Fix deployment script to address read-only variable reuse
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -6,11 +6,12 @@ set -o pipefail
 
 export PATH="/opt/go/1.19.5/bin:${PATH}"
 
-BASE_IMG="gabi"
-QUAY_IMAGE="quay.io/app-sre/${BASE_IMG}"
-IMG="${BASE_IMG}:latest"
+BASE_IMAGE="gabi"
+QUAY_IMAGE="quay.io/app-sre/${BASE_IMAGE}"
 
-readonly BASE_IMG QUAY_IMAGE IMG
+TARGET_IMAGE="${BASE_IMAGE}:latest"
+
+readonly BASE_IMAGE QUAY_IMAGE TARGET_IMAGE
 
 GIT_HASH=$(git rev-parse --short=7 HEAD)
 
@@ -19,15 +20,15 @@ GIT_HASH=$(git rev-parse --short=7 HEAD)
     docker login quay.io -u "${QUAY_USER}" -p "${QUAY_TOKEN}"
 }
 
-BUILD_CMD="docker build" IMG="${IMG}" make docker-build
+BUILD_CMD="docker build" IMG="${TARGET_IMAGE}" make docker-build
 
 {
     set +x
     skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-        "docker-daemon:${IMG}" \
+        "docker-daemon:${TARGET_IMAGE}" \
         "docker://${QUAY_IMAGE}:latest"
 
     skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-        "docker-daemon:${IMG}" \
+        "docker-daemon:${TARGET_IMAGE}" \
         "docker://${QUAY_IMAGE}:${GIT_HASH}"
 }
