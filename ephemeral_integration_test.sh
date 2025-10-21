d21ab6d94391fc1c8880558c24354f5aa288cf21
Esron Silva <essilva@redhat.com>
2025-10-21 12:20:51 -0300

    fix: use curl inside test pod

--- ephemeral_integration_test.sh
+++ ephemeral_integration_test.sh
@@ -9,10 +9,6 @@ echo "Running ephemeral integration test"
 echo "Image URL: ${IMAGE_URL}"
 echo "Image Digest: ${IMAGE_DIGEST}"
 
-echo "Getting current namespace..."
-NAMESPACE=$(oc project -q)
-echo "Namespace: ${NAMESPACE}"
-
 echo "Creating pod YAML..."
 oc process -f test/test-pod-template.yml -o yaml \
     -p IMAGE_URL="${IMAGE_URL}" \
@@ -34,20 +30,11 @@ echo "Getting pod information..."
 oc get pods my-test-app
 
 echo ""
-echo "Getting service information..."
-oc get service my-test-app-internal
-
-echo ""
-echo "Waiting a moment for DNS propagation..."
-sleep 3
-
-echo ""
 echo "Testing pod with curl..."
-curl --silent --show-error --fail "http://my-test-app-internal.${NAMESPACE}.svc.cluster.local:8080/healthcheck"
+oc exec my-test-app -c app -- curl --silent --show-error --fail "http://localhost:8080/healthcheck"
 
 echo ""
 echo "Cleaning up..."
 oc delete pod/my-test-app
-oc delete service/my-test-app-internal
 
 exit 0
