935f9be3a447c9e918edab333effa90a0bd66320
Esron Silva <essilva@redhat.com>
2025-10-31 14:50:55 -0300

    fix: create token with correct content type

--- test/helper_test.go
+++ test/helper_test.go
@@ -4,7 +4,6 @@
 package test
 
 import (
-	"bytes"
 	"context"
 	"crypto/tls"
 	"database/sql"
@@ -13,8 +12,10 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -54,11 +55,16 @@ func createConfigurationFile(t *testing.T, expiration time.Time, users []string)
 func createSplunkIngestToken(t *testing.T, client http.Client, host, port, password string) string {
 	splunkURL := fmt.Sprintf("https://%s:%s/servicesNS/admin/splunk_httpinput/data/inputs/http?output_mode=json", host, port)
 
-	req, err := http.NewRequest(http.MethodPost, splunkURL, bytes.NewBufferString(`name=mytokexna`))
+	// Use url.Values for proper form encoding
+	data := url.Values{}
+	data.Set("name", "mytokexna")
+
+	req, err := http.NewRequest(http.MethodPost, splunkURL, strings.NewReader(data.Encode()))
 	if err != nil {
 		t.Fatal(err)
 	}
 	req.SetBasicAuth("admin", password)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	resp, err := client.Do(req)
 	if err != nil {
